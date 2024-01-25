483c204c5964406039408957ba7fcc0bdded64a1
Jun <jun.coder95@gmail.com>
2024-01-25 12:55:16 +0700

    feat: change admin account

--- public/src/parent/load.ts
+++ public/src/parent/load.ts
@@ -17,7 +17,7 @@ limitations under the License.
 import { isMobile } from "./common";
 import { toggleIframe } from "./iframe";
 
-const parentHostRoot = ( document.querySelector("#chatterbox-script") as HTMLScriptElement).src;
+const parentHostRoot = (document.querySelector("#chatterbox-script") as HTMLScriptElement).src;
 const hostRoot = new URL(parentHostRoot).origin;
 
 export function loadStartButton() {
@@ -41,7 +41,7 @@ function createStartButton() {
     const button = document.createElement("button");
     button.className = "start-chat-btn";
     button.setAttribute('aria-label', 'Start chat');
-    button.onclick = () => (window as any).isIframeLoaded? toggleIframe() : loadIframe();
+    button.onclick = () => (window as any).isIframeLoaded ? toggleIframe() : loadIframe();
     button.appendChild(createNotificationBadge());
     return button;
 }
@@ -66,13 +66,13 @@ function loadIframe(minimized = false) {
         throw new Error("CHATTERBOX_CONFIG_LOCATION is not set");
     }
     iframe.src = new URL(
-        `../chatterbox.html?config=${configLocation}${minimized? "&minimized=true": ""}`,
+        `../chatterbox.html?config=${configLocation}${minimized ? "&minimized=true" : ""}`,
         hostRoot
     ).href;
     iframe.className = "chatterbox-iframe";
     document.body.appendChild(iframe);
     (window as any).isIframeLoaded = true;
-    document .querySelector(".start-chat-btn") .classList.add("start-background-minimized");
+    document.querySelector(".start-chat-btn")?.classList.add("start-background-minimized");
     if (isMobile()) {
         (document.querySelector(".start") as HTMLDivElement).style.display =
             "none";
